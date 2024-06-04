9b32380ad93a72309fbde25c9c3428b8736a1347
Muhammad Yasir <myasir47991@gmail.com>
2024-06-04 15:46:31 +0500

    chore: Seed database with chat data and messages

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import chatRouter from './routes/chat.route.js';
 import { errorMiddleware } from './middlewares/error.middleware.js';
+// import { createMessagesInAChat, createSingleChats } from './seeders/chat.seeders.js';
 
 
 
@@ -16,6 +17,8 @@ dotenv.config({
 const PORT = process.env.PORT || 8000;
 connectDB();
 // createUser(10); // Uncomment this line to seed the database with 10 fake users
+// createSingleChats(2); // Uncomment this line to seed the database with 1 single chats
+// createMessagesInAChat("665c1599ab2bf0c6476b51bf", 50); // Uncomment this line to seed the database with 50 messages in a specific chat
 
 const app = express();
 app.use(express.json());

ba45743f05e428ffbf1e3f2c36bd6b16130ea720
Muhammad Yasir <myasir47991@gmail.com>
2024-06-19 11:51:38 +0500

    ing user information in a list

--- Frontend/src/components/specific/Search.jsx
+++ Frontend/src/components/specific/Search.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import { useInputValidation } from "6pp";
+import { Search as SearchIcon } from "@mui/icons-material";
+import {
+  Dialog,
+  DialogTitle,
+  InputAdornment,
+  List,
+  Stack,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+import UserItem from "../shared/UserItem";
+import { sampleUsers } from "../../constants/sampleData";
 
 const Search = () => {
-  return (
-    <div>
-      Search
-    </div>
-  )
-}
 
-export default Search
+  const search = useInputValidation("");
+
+  const [users, setUsers] = useState([]);
+
+  const isLoadingSendFriendRequest = false;
+
+  const addFriendHandler = async (id) => {
+    console.log("Sending friend request");
+  };
+
+  return (
+    <Dialog open>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant="outlined"
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <List>
+          {sampleUsers.map((i) => (
+            <UserItem
+              user={i}
+              key={i._id}
+              handler={addFriendHandler}
+              handlerIsLoading={isLoadingSendFriendRequest}
+            />
+          ))}
+        </List>
+      </Stack>
+    </Dialog>
+  );
+};
+
+export default Search;

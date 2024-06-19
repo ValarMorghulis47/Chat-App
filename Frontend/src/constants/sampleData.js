fba8ad5190c4b5129157391013bd6479a11ea032
Muhammad Yasir <myasir47991@gmail.com>
2024-06-19 12:07:53 +0500

    notifications done

--- Frontend/src/constants/sampleData.js
+++ Frontend/src/constants/sampleData.js
@@ -29,3 +29,20 @@ export const sampleUsers = [
     _id: "2",
   },
 ];
+
+export const sampleNotifications = [
+  {
+    sender: {
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      name: "John Doe",
+    },
+    _id: "1",
+  },
+  {
+    sender: {
+      avatar: "https://www.w3schools.com/howto/img_avatar2.png",
+      name: "John Boi",
+    },
+    _id: "2",
+  },
+];
